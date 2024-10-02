5e3ad885893514d83523327d2b4fd80f63250d07
Paulius <paulius.giniunas@gmail.com>
2024-10-02 12:32:11 +0300

    fixes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 xcode-select --install
 
 ### Get .dotfiles
-git clone --recursive git@github.com:pauliusgin/dotfiles.git ~/.dotfiles
+git clone --recursive https://github.com/pauliusgin/dotfiles.git ~/.dotfiles
 
 ### Install
 ~/.dotfiles/boot.sh
--- boot.sh
+++ boot.sh
@@ -3,7 +3,7 @@
 echo "Setting up your Mac..."
 
 # Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
+if ! command -v brew &> /dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
