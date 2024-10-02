9dc319c35a847d7df4da934a3ce6c75b4e1e7593
Paulius <paulius.giniunas@gmail.com>
2024-10-02 11:56:19 +0300

    various updates

---
+++ README.md
@@ -0,0 +1,8 @@
+### Get Git
+xcode-select --install
+
+### Get .dotfiles
+git clone --recursive git@github.com:pauliusgin/dotfiles.git ~/.dotfiles
+
+### Install
+~/.dotfiles/boot.sh
--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 echo "Setting up your Mac..."
 
@@ -25,15 +25,17 @@ mkdir $HOME/Documents/Books
 mkdir $HOME/Documents/Learn\ To\ Code
 mkdir $HOME/Documents/Personal
 mkdir $HOME/Work
+mkdir $HOME/Pictures/Wallpapers
 
 
-# Symlink configs
+# Symlinks
 ln -swf $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -swf $HOME/.dotfiles/kitty.conf $HOME/.config/kitty/kitty.conf
 ln -swf $HOME/.dotfiles/init.lua $HOME/.config/nvim/init.lua
 ln -swf $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
 ln -swf $HOME/.dotfiles/code/extensions.json $HOME/.vscode/extensions/extensions.json
 ln -swf $HOME/.dotfiles/.cspell.json $HOME/.cspell.json
+sudo ln -sf $HOME/.dotfiles/wallpaper/kitty-gattsu.png $HOME/Pictures/Wallpapers/gattsu_evil_01.png
 
 #Npm packs
 npm i -g cspell @cspell/dict-lt-lt @cspell/dict-lua eslint prettier typescript typescript-language-server ts-node
